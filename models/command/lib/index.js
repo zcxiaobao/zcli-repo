4b6da5cab3c578c5b3c959a2e86d7690c9d4b792
zcxiaobao <444186976@qq.com>
2023-12-01 14:51:47 +0800

    feat:models/command create

---
+++ models/command/lib/index.js
@@ -0,0 +1,32 @@
+class Command {
+  constructor(argv) {
+    const runner = new Promise((resolve, reject) => {
+      let chain = Promise.resolve();
+      chain = chain.then(() => this.configeEnv());
+      chain = chain.then(() => this.initArgs());
+      chain = chain.then(() => this.runProperties());
+      chain = chain.then(() => this.runCommand());
+      chain.then(() => resolve);
+      chain.catch((err) => {
+        console.log(err);
+        reject(err);
+      });
+    });
+    this.runner = runner;
+  }
+  initArgs() {}
+  runProperties() {}
+  async runCommand() {
+    const proceed = await this.initialize();
+    if (proceed !== false) {
+      return this.execute();
+    }
+    return undefined;
+  }
+  initialize() {
+    throw new Error("initialize must be implemented");
+  }
+  execute() {
+    new Error("execute must be implemented");
+  }
+}
